68e2a2dd6658fe5739ddcc37495719a2f1098153
Grant Jarvis <grantjarvis8@gmail.com>
2021-02-08 19:42:41 -0600

    Initial Commit

---
+++ Sound Localization/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Sound Localization
+//
+//  Created by Grant Jarvis on 2/8/21.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
